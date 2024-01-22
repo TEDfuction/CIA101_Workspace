ab4226c6b48e5bd593b09362669fcb7fd9b4621e
shengyang <hh870150@gmail.com>
2024-01-22 21:01:32 +0800

    second commit

--- src/hw3/javahw3.java
+++ src/hw3/javahw3.java
@@ -1,12 +1,12 @@
 package hw3;
 
 public class javahw3 {
-
+	
 	public static void main(String[] args) {
 		double x[][] = new double[3][3];
 		double y[][] = new double[3][3];
 		double z[][] = new double[3][3];
-		
+		System.out.println("git測試JAVA很好玩");
 		for (int i = 0; i < x.length; i++) {
 			for (int j = 0; j < x[i].length; j++) {
 				x[i][j] = (int) (Math.random() * 31);
@@ -21,7 +21,7 @@ public class javahw3 {
 				System.out.print("y" + y[i][j] + "\t");
 			}
 			System.out.println();
-			
+
 		}
 		System.out.print("=====================\n");
 		for (int i = 0; i < z.length; i++) {
