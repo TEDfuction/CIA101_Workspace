ef5404003f624f67f451240c469469f25ba951ce
shengyang <hh870150@gmail.com>
2024-01-22 21:05:37 +0800

    second commit javahw2

--- src/hw2/Javahw2.java
+++ src/hw2/Javahw2.java
@@ -88,7 +88,7 @@ public class Javahw2 {
 //			}
 //			System.out.println();
 //		}
-		// -----------請設計一隻Java程式,輸出結果為以下:A BB CCC DDD EEE
+		// -----------請設計一隻Java程式,輸出結果為以下:A BB CCC DDD EEE--//
 //		char k = 65;
 //		for (int i = 1; i <= 6; i++) {
 //			for (int j = 1; j <= i; j++) {
